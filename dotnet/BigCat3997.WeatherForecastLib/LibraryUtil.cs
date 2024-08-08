18987fe12e83b7b7dc9a709894fbe76fdaada640
bigcat3997 <vqt.develop@gmail.com>
2024-08-08 16:26:07 +0700

    add LibraryUtil

---
+++ dotnet/BigCat3997.WeatherForecastLib/LibraryUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace BigCat3997.DemoLibrary.Dotnet6;
+
+public class LibraryUtil
+{
+    public static String showMessage(string message)
+    {
+        Console.WriteLine(message);
+        return message;
+    }
+}
