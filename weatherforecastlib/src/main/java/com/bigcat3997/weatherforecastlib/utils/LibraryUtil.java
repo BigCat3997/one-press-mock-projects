9efeb25a047655ac1f300722a2a388e4bf2bbf71
bigcat3997 <vqt.develop@gmail.com>
2024-08-08 15:47:08 +0700

    add new project weatherforecastlib

---
+++ weatherforecastlib/src/main/java/com/bigcat3997/weatherforecastlib/utils/LibraryUtil.java
@@ -0,0 +1,8 @@
+package com.bigcat3997.weatherforecastlib.utils;
+
+public final class LibraryUtil {
+    public static String showMessage(String message) {
+        System.out.println(message);
+        return message;
+    }
+}
