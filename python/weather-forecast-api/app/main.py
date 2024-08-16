1336a2a0a4117be3b5987525364f2e8492830050
bigcat3997 <vqt.develop@gmail.com>
2024-08-16 17:06:14 +0700

    refactor some configs when start the app

--- python/weather-forecast-api/app/main.py
+++ python/weather-forecast-api/app/main.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+import configparser
+import logging
 import os
+from datetime import datetime
 
+import uvicorn
 from azure.identity import ClientSecretCredential
 from azure.keyvault.secrets import SecretClient
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
-import logging
-import configparser
 
 # Specify the directory name
 dir_name = "logs"
@@ -17,22 +17,24 @@ if not os.path.exists(dir_name):
     # If the directory doesn't exist, create it
     os.makedirs(dir_name)
 
-logger = logging.getLogger('my_logger')
+logger = logging.getLogger("my_logger")
 logger.setLevel(logging.INFO)
 logging_file_name = "application-{:%Y%m%d}.log".format(datetime.now())
-handler = logging.FileHandler(f'logs/{logging_file_name}')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.FileHandler(f"logs/{logging_file_name}")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.info('Bootstrap the application...')
+logger.info("Bootstrap the application...")
 
 config = configparser.ConfigParser()
 
-config.read('resources/config.ini')
+config.read("resources/config.ini")
 
-app_name = config.get('CONFIGURATION', 'APP_NAME', fallback='Simple API')
-app_version = config.get('CONFIGURATION', 'APP_VERSION', fallback='1.0.0')
-sub_path = config.get('CONFIGURATION', 'SUB_PATH', fallback='')
+app_name = config.get("CONFIGURATION", "APP_NAME", fallback="Simple API")
+app_version = config.get("CONFIGURATION", "APP_VERSION", fallback="1.0.0")
+app_port = config.getint("CONFIGURATION", "APP_PORT", fallback=8000)
+sub_path = config.get("CONFIGURATION", "SUB_PATH", fallback="")
+app_workers = config.getint("CONFIGURATION", "APP_WORKERS", fallback=1)
 
 app = FastAPI(
     docs_url=f"{sub_path}/swagger-ui",
@@ -53,13 +55,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get(f"{sub_path}/")
 async def root():
     return {"message": f"Hello {app_name}!"}
 
+
 @app.get(f"{sub_path}/version")
 async def version():
     return {"name": f"{app_name}", "version": app_version}
+
 
 @app.get(f"{sub_path}/vaults/secrets")
 async def list_secrets():
@@ -68,20 +73,23 @@ async def list_secrets():
     vault_client_id = os.getenv("VAULT_CLIENT_ID", "")
     vault_client_secret = os.getenv("VAULT_CLIENT_SECRET", "")
 
-    credential = ClientSecretCredential(tenant_id=vault_tenant_id, client_id=vault_client_id, client_secret=vault_client_secret)
+    credential = ClientSecretCredential(
+        tenant_id=vault_tenant_id,
+        client_id=vault_client_id,
+        client_secret=vault_client_secret,
+    )
 
     secret_client = SecretClient(vault_url=vault_url, credential=credential)
-    secret_names = [secret.name for secret in secret_client.list_properties_of_secrets()]
+    secret_names = [
+        secret.name for secret in secret_client.list_properties_of_secrets()
+    ]
     print(secret_names)
     return secret_names
 
+
 if __name__ == "__main__":
-    import uvicorn
+    print(f"The application are running with {app_workers} workers at {app_port} port.")
 
-    worker = os.getenv("WORKER", 4)
-    app_port = os.getenv("APP_PORT", 8000)
-    print(
-        f"AZURE INTEGRATION APPLICATION ARE RUNNING WITH {worker} WORKERS AT PORT {app_port}."
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=app_port, workers=app_workers, reload=False
     )
-
-    uvicorn.run("main:app", host="0.0.0.0", port=app_port, workers=worker, reload=False)
