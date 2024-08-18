b98b28fba090c815eeb5c9acf411a31fe164279b
bigcat3997 <vqt.develop@gmail.com>
2024-08-18 12:54:03 +0700

    add unit test for the project

--- maven/weather-forecast-app/src/test/java/com/bigcat3997/weatherforecastapp/WeatherforecastappApplicationTests.java
+++ maven/weather-forecast-app/src/test/java/com/bigcat3997/weatherforecastapp/WeatherforecastappApplicationTests.java
@@ -1,5 +1,7 @@
 package com.bigcat3997.weatherforecastapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,19 @@ class WeatherforecastappApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testAddition() {
+		var a = 1;
+		var b = 2;
+		var result = a + b;
+		assertEquals(3, result, "1 + 2 should equal 3");
+	}
+
+	@Test
+	void testSubtraction() {
+		var a = 1;
+		var b = 2;
+		var result = a - b;
+		assertEquals(-1, result, "1 - 2 should equal -1");
+	}
 }
